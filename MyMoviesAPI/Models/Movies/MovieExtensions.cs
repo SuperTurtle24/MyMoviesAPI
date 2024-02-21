2ddbfd01d984e7cf79189b748afef786f62842b0
Joe Edwards <joe.edwards@greenlordstudios.co.uk>
2024-02-21 15:35:07 +0000

    Gave the extension method some default values

--- MyMoviesAPI/Models/Movies/MovieExtensions.cs
+++ MyMoviesAPI/Models/Movies/MovieExtensions.cs
@@ -1,7 +1,7 @@
 ﻿namespace MyMoviesAPI.Models.Movies;
 public static class MovieExtensions
 {
-    public static IQueryable<Movie> FilterMovies(this IQueryable<Movie> movies, string? genre, string? title)
+    public static IQueryable<Movie> FilterMovies(this IQueryable<Movie> movies, string? genre = null, string? title = null)
     {
         if (genre != null) movies = movies.Where(x => x.Genre.ToLower().Contains(genre.ToLower()));
         if (title != null) movies = movies.Where(x => x.Title.ToLower().Contains(title.ToLower()));
